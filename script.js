b015d9a7d5ce75ed778d92ad5976b09b57ced70c
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-09-19 14:42:13 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mySidenav").setAttribute("style", "width:250px;box-shadow:1px 1px 1px 1px #4e4e4e;");
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").setAttribute("style", "width:0px;box-shadow:initial;");
+}
